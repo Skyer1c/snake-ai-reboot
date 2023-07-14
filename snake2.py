988c4831803885e730f029ce1b12dbe353913698
LJX2017 <48950301+LJX2017@users.noreply.github.com>
2023-07-14 13:28:24 +0800

    Add files via upload
    updated snake

--- snake2.py
+++ snake2.py
@@ -14,32 +14,32 @@ green = (0, 255, 0)
 blue = (50, 153, 213)
 
 # Set the width and height of the game window
-width_time = 30
-height_time = 30
-snake_block = 12
-dis_width = width_time*snake_block
-dis_height = height_time*snake_block
-
-dis = pygame.display.set_mode((dis_width, dis_height))
 
 # Set the title of the game window
 pygame.display.set_caption('Snake Game')
 
 clock = pygame.time.Clock()
 
-
+snake_block = 100
 snake_speed = 5
 
+block_count=12
+
+dis_width = snake_block*block_count
+dis_height = snake_block*block_count
+dis = pygame.display.set_mode((dis_width, dis_height))
 font_style = pygame.font.SysFont(None, 50)
 score_font = pygame.font.SysFont(None, 35)
 
 
 def our_snake(snake_block, snake_list):
-    for i in range(len(snake_list)):
-        if(i==len(snake_list)-1):
-            pygame.draw.rect(dis, blue, [snake_list[i][0], snake_list[i][1], snake_block, snake_block])
-            continue
-        pygame.draw.rect(dis, green, [snake_list[i][0], snake_list[i][1], snake_block, snake_block])
+    cnt=0
+    for x in snake_list[::-1]:
+        cnt+=1
+        if cnt==1:
+            pygame.draw.rect(dis, red, [x[0], x[1], snake_block, snake_block])
+        else:
+            pygame.draw.rect(dis, green, [x[0], x[1], snake_block, snake_block])
 
 
 def message(msg, color):
@@ -51,8 +51,8 @@ def game_loop():
     game_over = False
     game_close = False
 
-    x1 = dis_width/2
-    y1 = dis_height/2
+    x1 = block_count//2*snake_block
+    y1 = block_count//2*snake_block
 
     x1_change = 0
     y1_change = 0
