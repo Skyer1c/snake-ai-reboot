5fba226d30c3fbee281349e3a456370fc5ccbc5d
LJX2017 <48950301+LJX2017@users.noreply.github.com>
2023-07-17 16:06:12 +0800

    Add files via upload

--- DQN_model.py
+++ DQN_model.py
@@ -5,14 +5,17 @@ import torch.nn.functional as F
 import os
 
 class Linear_QNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden1_size,hidden2_size, output_size):
         super().__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, output_size)
-
+        self.linear1 = nn.Linear(input_size, hidden1_size)
+        self.dropout = nn.Dropout(0.7)
+        self.linear2 = nn.Linear(hidden1_size, hidden2_size)
+        self.linear3 = nn.Linear(hidden2_size, output_size)
     def forward(self, x):
         x = F.relu(self.linear1(x))
-        x = self.linear2(x)
+        x = F.relu(self.dropout(x))
+        x = F.relu(self.linear2(x))
+        x=self.linear3(x)
         return x
 
     def save(self, file_name='model.pth'):
